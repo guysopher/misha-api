cec841cb1a2cd64b91a9460a0452321a62dde8e0
Guy Sopher <GuySopher@gmail.com>
2015-12-15 16:30:23 +0200

    fixing issue (oded the king)

--- config/connections.js
+++ config/connections.js
@@ -28,13 +28,8 @@ module.exports.connections = {
   * Installed by default.                                                    *
   *                                                                          *
   ***************************************************************************/
-  // localDiskDb: {
-  //   adapter: 'sails-disk'
-  // },
-
-  'heroku-mongolab': {
-    adapter: 'sails-mongo',
-    url: process.env.MONGOLAB_URI
+  localDiskDb: {
+    adapter: 'sails-disk'
   },
 
   /***************************************************************************
@@ -61,13 +56,9 @@ module.exports.connections = {
   * Run: npm install sails-mongo                                             *
   *                                                                          *
   ***************************************************************************/
-  someMongodbServer: {
+  'heroku-mongolab': {
     adapter: 'sails-mongo',
-    host: 'localhost',
-    port: 27017,
-    // user: 'username',
-    // password: 'password',
-    // database: 'your_mongo_db_name_here'
+    url: process.env.MONGOLAB_URI
   },
 
   /***************************************************************************
--- config/env/development.js
+++ config/env/development.js
@@ -18,7 +18,7 @@ module.exports = {
    ***************************************************************************/
 
   models: {
-    connection: 'heroku-mongolab'
+    connection: 'localDiskDb'
   }
 
 };
