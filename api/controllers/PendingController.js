16a3b7a1a13be0e72736663bb34c76fc4d11cf52
Guy Sopher <GuySopher@gmail.com>
2015-12-16 11:58:13 +0200

    add pending api

---
+++ api/controllers/PendingController.js
@@ -0,0 +1,13 @@
+/**
+ * PendingController
+ *
+ * @description :: Server-side logic for managing pendings
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+
+
+};
+
---
+++ api/models/Pending.js
@@ -0,0 +1,16 @@
+/**
+* Pending.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+    user_id: String,
+    waiting_for: String,
+    message_id: String
+  }
+};
+
