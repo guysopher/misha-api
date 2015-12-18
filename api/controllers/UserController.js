49e9959f923477e9981874f7feeef038284e485d
Guy Sopher <GuySopher@gmail.com>
2015-12-18 20:15:47 +0200

    add support for email invites

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -20,6 +20,19 @@ module.exports = {
     User.update({id: req.query.user_id}, user).exec(function (err, updated) {
       sails.controllers.pending.checkMessages(req, res, next);
     });
+  },
+
+  invite: function(req, res, next) {
+    sails.hooks.email.send('', {
+      name: (req.body && req.body.name) || (req.query && req.query.name)
+    }, {
+      to: (req.body && req.body.email) || (req.query && req.query.email),
+      from: 'misha@wix.com',
+      subject: 'misha.m'
+    }, function(err, data) {
+      res.send({err:err, data:data});
+      console.log('email sent?', err, data);
+    })
   }
 
 };
---
+++ config/email.js
@@ -0,0 +1,12 @@
+module.exports.email = {
+
+  service: "Mailgun",
+  auth: {
+    user:'postmaster@sandboxd2bdd33b0cd8414f8023757a15daeb69.mailgun.org',
+    pass: '1800d405e345fe0010603a356c07909e'
+  },
+  from: 'misha@wix.com',
+  testMode: false,
+  alwaysSendTo: 'guyso@wix.com'
+
+};
